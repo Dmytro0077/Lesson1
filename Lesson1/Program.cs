2c3c5106379d7dcc3d168885b37689245f4b89d3
Dmytro0077 <kaktusgren@gmail.com>
2022-08-01 18:12:20 +0300

    secondmerge

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,3 +1,6 @@
 ﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("How are you!");
-Console.WriteLine("I'm good!");
+Console.WriteLine("I'm good! and you");
+Console.WriteLine("I'm fine, becauce Ukraine won");
+Console.WriteLine("It's good information!");
+Console.WriteLine("I agree!");
